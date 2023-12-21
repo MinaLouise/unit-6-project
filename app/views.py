6a7dd2d5cdce0b78a3b7547701ba1de6be4e105b
BlaineLambert <flambert24@basecampcodingacademy.org>
2023-12-21 10:04:45 -0600

    homepage and login fixed

--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from .forms import *
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def register(request):
-    form = CreateUserForm()
+    form = CreateAccount()
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
+        form = CreateAccount(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
@@ -25,11 +25,11 @@ def loginpage(request):
         if user is not None:
             login(request, user)
             return redirect('register')
-        else:
-            return render(request, 'login.html')
+
     else:
         
         return render(request, 'login.html')
+
 
 def homepage(request):
     return render(request, 'home.html')
