11bf467e31904cc61940b0b21c761655a7efdccb
Abmccullough1 <wmccullough24@basecampcodingacademy.com>
2024-01-08 13:28:15 -0600

    add some to model

--- app/models.py
+++ app/models.py
@@ -9,8 +9,8 @@ class Account(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length = 10)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
+    profile_picture = models.ImageField(null=True, blank=True)
+    
 
 class Properties(models.Model):
     user_props = models.ForeignKey(Account, on_delete=models.CASCADE)
