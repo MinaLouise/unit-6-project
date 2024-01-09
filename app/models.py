75fe6b7aeb6ebdbbab6582f1dabf040e0dc8556d
Abmccullough1 <wmccullough24@basecampcodingacademy.com>
2024-01-09 13:20:44 -0600

    new migrations

--- app/models.py
+++ app/models.py
@@ -11,10 +11,11 @@ class Account(models.Model):
 class Properties(models.Model):
     user_props = models.ForeignKey(Account, on_delete=models.CASCADE)
     price = models.IntegerField()
-    address = models.TextField()
-    city = models.TextField()
+    address = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
     zip_code = models.IntegerField()
     size = models.IntegerField()
     available = models.BooleanField()
+    picture = models.ImageField(null=True, blank=True)
 
 
